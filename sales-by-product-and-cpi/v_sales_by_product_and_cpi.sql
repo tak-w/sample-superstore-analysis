927c5a0b30e0d490422520d36d8fa5a8d4a87840
tak-w <60606451+tak-w@users.noreply.github.com>
2024-01-17 11:15:44 +0900

    Create v_sales_by_product_and_cpi.sql

---
+++ sales-by-product-and-cpi/v_sales_by_product_and_cpi.sql
@@ -0,0 +1,34 @@
+with cpi_monthly as (
+  select
+    date(
+      concat(
+        substr(cast(month as string), 1, 4), 
+        '-',
+        substr(cast(month as string), 5, 2),
+        '-01'
+      )
+    ) as month,
+    cpi
+  from `sample-superstore-411114.external_data.cpi_tokyo_monthly`
+  order by month 
+)
+
+select
+  date_trunc(order_date, month) as order_month,
+  city,
+  prefecture,
+  region,
+  product_category,
+  product_subcategory,
+  product_name,
+  sales,
+  quantity,
+  profit,
+  discount_rate,
+  c.cpi
+from
+  `sample-superstore-411114.sample_superstore.orders` as o
+  inner join cpi_monthly as c
+  on date_trunc(o.order_date, month) = c.month
+order by
+  order_month
